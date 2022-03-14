4c551b7a9f81115fbc8ff81a4b314af390bc47db
xDmv <makushin.dima@gmail.com>
2022-03-14 20:45:55 +0200

    update pages

--- src/app/pages/questions-management/questions-management.component.ts
+++ src/app/pages/questions-management/questions-management.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { StateAppService } from '../../servises/state-app.service';
+import { Question } from '../../share/interfaces/question';
 
 @Component({
   selector: 'app-questions-management',
   templateUrl: './questions-management.component.html',
   styleUrls: ['./questions-management.component.scss']
 })
-export class QuestionsManagementComponent implements OnInit {
+export class QuestionsManagementComponent {
+  public questions: Question[] = [];
 
-  constructor() { }
+  constructor(public storage: StateAppService) {}
 
-  ngOnInit(): void {
+  ngAfterContentInit() {
+    this.storage.init();
+    this.questions = this.storage.getQuestion();
   }
 
+  onDeleteQuestion(index: number) {
+    this.questions.splice(index, 1);
+    this.storage.deleteQuestion(this.questions);
+  }
 }
