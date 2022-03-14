7ac313f569aca8c402828462c6f467aaa3e82e2a
xDmv <makushin.dima@gmail.com>
2022-03-14 20:57:44 +0200

    update state-app service

--- src/app/servises/state-app.service.ts
+++ src/app/servises/state-app.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
+import { Question } from '../share/interfaces/question';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StateAppService {
+  private questions!: Question[];
 
-  constructor() { }
+  constructor() {
+    this.questions = [];
+  }
+
+  init() {
+    if (localStorage.getItem('questions')) {
+      return;
+    }
+    const element = `${localStorage.getItem('questions')}`;
+    this.questions = JSON.parse(element);
+  }
+
+  setQuestion(value: Question) {
+    this.questions.push(value);
+    localStorage.setItem('questions', JSON.stringify(this.questions));
+  }
+
+  getQuestion(): Question[] {
+    const element = `${localStorage.getItem('questions')}`;
+    this.questions = JSON.parse(element);
+    return this.questions;
+  }
+
+  editQuestion() {
+
+  }
+
+  deleteQuestion(value: Question[]) {
+    this.questions = [];
+    this.questions = value;
+    localStorage.removeItem('questions');
+    localStorage.setItem('questions', JSON.stringify(this.questions));
+  }
 }
