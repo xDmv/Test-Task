a21bc9b26ebff6d1f4aff65ec948ed80ce3b1776
xDmv <makushin.dima@gmail.com>
2022-03-14 12:28:16 +0200

    create default routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
-    loadChildren: () => import('./pages/question-management/question-management.module').then(m => m.QuestionManagementModule)
+    path: 'questions-management',
+    loadChildren: () => import('./pages/questions-management/questions-management.module').then(m => m.QuestionsManagementModule)
+  },
+  {
+    path: 'create-question',
+    loadChildren: () => import('./pages/create-question/create-question.module').then(m => m.CreateQuestionModule)
+  },
+  {
+    path: 'edit-question/:id',
+    loadChildren: () => import('./pages/edit-question/edit-question.module').then(m => m.EditQuestionModule)
+  },
+  {
+    path: 'list-questions',
+    loadChildren: () => import('./pages/list-of-questions/list-of-questions.module').then(m => m.ListOfQuestionsModule)
   }
 ];
 
