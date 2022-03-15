500aeadefd8f7963c2f16cec8e40e10850e2b514
xDmv <makushin.dima@gmail.com>
2022-03-15 19:25:14 +0200

    update 'Create question Page'

--- src/app/pages/create-question/create-question.component.ts
+++ src/app/pages/create-question/create-question.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Question } from "../../share/interfaces/question";
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Question } from '../../share/interfaces/question';
 import { Answers } from '../../share/interfaces/answers';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { StateAppService } from '../../servises/state-app.service';
 import * as dayjs from 'dayjs'
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -18,17 +19,23 @@ export class CreateQuestionComponent implements OnInit {
 
   constructor(
     public formBuilder : FormBuilder,
-    public storage: StateAppService
+    public storage: StateAppService,
+    private route: Router
   ) {
     this.formQuestion = this.formBuilder.group({
       title: [null, [Validators.required, Validators.maxLength(255), Validators.minLength(1)]],
       typeQuestion: ['single', [Validators.required]],
       answers: this.formBuilder.array([])
-    })
+    });
   }
 
   ngOnInit(): void {
     this.addAnswer();
+    this.storage.init();
+  }
+
+  ngAfterContentInit() {
+    this.storage.init();
   }
 
   public addAnswer(answer: string = '') {
@@ -51,18 +58,22 @@ export class CreateQuestionComponent implements OnInit {
       }
     );
     this.question = {
+      id: Date.now(),
       title: this.formQuestion.controls['title'].value,
       type: this.formQuestion.controls['typeQuestion'].value,
       isRead: false,
       createDate: dayjs().format('YYYY-MM-DD HH:mm:ss'),
+      answerDate: 0,
       answers: this.formQuestion.controls['answers'].value
     };
     if (this.formQuestion.controls['typeQuestion'].value === 'open') {
       this.question = {
+        id: Date.now(),
         title: this.formQuestion.controls['title'].value,
         type: this.formQuestion.controls['typeQuestion'].value,
         isRead: false,
-        createDate: dayjs().format('YYYY-MM-DD HH:mm:ss') ,
+        createDate: dayjs().format('YYYY-MM-DD HH:mm:ss'),
+        answerDate: 0,
         answers: [
           {
             answer: '',
@@ -71,12 +82,11 @@ export class CreateQuestionComponent implements OnInit {
         ]
       };
     }
-    console.log('question: ', this.question);
     this.storage.setQuestion(this.question);
+    this.route.navigateByUrl('/questions-management').then();
   }
 
   public deleteAnswer(i: number) {
-    console.log('delete answer: ', i);
     let item: FormArray = this.formQuestion.get('answers') as FormArray;
     item.removeAt(i);
   }
